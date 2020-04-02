3a195569ff5c95f99c720478805a461ec909e3d9
Yehor Windows <egorkavin221100@gmail.com>
2020-04-02 22:03:26 +0300

    Ready

--- js/main.js
+++ js/main.js
@@ -2,10 +2,16 @@ changeHeightAfterClick('bar__more', '100%');
 changeHeightAfterClick('detailedBar__closeButton', '0');
 function changeHeightAfterClick(className, height) {
 	let classList = document.getElementsByClassName(className);
+
 	for (let i = 0; i < classList.length; i++) {
 		classList[i].addEventListener('click', function() {
 			let overlay = document.getElementsByClassName('overlay');
 			overlay[i].style.height = height;
+			if (height == '100%') {
+				document.getElementsByTagName('body')[0].style.overflow = 'hidden';
+			} else {
+				document.getElementsByTagName('body')[0].style.overflow = 'visible';
+			}
 		});
 	}
 }
@@ -57,21 +63,6 @@ bartopItemActive.addEventListener('click', function() {
 	}
 });
 
-let moreButtons = document.getElementsByClassName('bar__moreIcons');
-for (let i = 0; i < moreButtons.length; i++) {
-	moreButtons[i].addEventListener('click', function() {
-		if (document.body.clientWidth >= 769) {
-			this.classList.toggle('bar__moreIcons_active');
-			let hiddenWindow = this.nextElementSibling;
-			if (hiddenWindow.style.display != 'flex') {
-				hiddenWindow.style.display = 'flex';
-			} else {
-				hiddenWindow.style.display = 'none';
-			}
-		}
-	});
-}
-
 let bar = document.getElementsByClassName('bar');
 for (let i = 0; i < bar.length; i++) {
 	bar[i].addEventListener('click', function() {
@@ -89,16 +80,32 @@ document.addEventListener('click', function(event) {
 	for (let i = 0; i < overlay.length; i++) {
 		if (event.target == overlay[i]) {
 			overlay[i].style.height = 0;
+			document.getElementsByTagName('body')[0].style.overflow = 'visible';
 		}
 	}
 	let barWindow = document.getElementsByClassName('bar__window');
 	let buttonToOpenBarWindow = document.getElementsByClassName('bar__moreIcons');
 	for (let i = 0; i < barWindow.length; i++) {
 		if (barWindow[i].style.display == 'flex') {
-			if (event.target != barWindow[i] && event.target != buttonToOpenBarWindow[i]) {
+			if (!barWindow[i].contains(event.target) && event.target != buttonToOpenBarWindow[i]) {
 				barWindow[i].style.display = 'none';
 				buttonToOpenBarWindow[i].classList.remove('bar__moreIcons_active');
 			}
+		}
+	}
+	let barInfo = document.getElementsByClassName('detailedBar__window');
+	let infoIcon = document.getElementsByClassName('detailedBar__infoIcon');
+	for (let i = 0; i < barInfo.length; i++) {
+		if (barInfo[i].style.display == 'flex') {
+			document.addEventListener('click', function(event2) {
+				if (barInfo[i].contains(event2.target)) {
+					barInfo[i].style.display = 'flex';
+					infoIcon[i].classList.add('detailedBar__infoIcon_active');
+				} else if (!infoIcon[i].contains(event2.target)) {
+					barInfo[i].style.display = 'none';
+					infoIcon[i].classList.remove('detailedBar__infoIcon_active');
+				}
+			});
 		}
 	}
 });
@@ -112,6 +119,21 @@ for (let i = 0; i < infoIcon.length; i++) {
 			hiddenWindow[i].style.display = 'flex';
 		} else {
 			hiddenWindow[i].style.display = 'none';
+		}
+	});
+}
+
+let moreButtons = document.getElementsByClassName('bar__moreIcons');
+for (let i = 0; i < moreButtons.length; i++) {
+	moreButtons[i].addEventListener('click', function() {
+		if (document.body.clientWidth >= 769) {
+			this.classList.toggle('bar__moreIcons_active');
+			let hiddenWindow = document.getElementsByClassName('bar__window');
+			if (hiddenWindow[i].style.display != 'flex') {
+				hiddenWindow[i].style.display = 'flex';
+			} else {
+				hiddenWindow[i].style.display = 'none';
+			}
 		}
 	});
 }
